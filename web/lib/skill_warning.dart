a451649b1dd9c527525fe74f83a6d9db53bff812
Alex Hartsell <kalhartt@gmail.com>
2013-09-09 13:27:29 -0400

    initial warning panel

--- web/lib/skill_warning.dart
+++ web/lib/skill_warning.dart
@@ -1,8 +1,19 @@
 library skill_warning;
 import 'package:polymer/polymer.dart';
-import 'models.dart';
+import 'dart:html';
 
 @CustomTag('skill-warning')
 class SkillWarning extends PolymerElement {
   bool get applyAuthorStyles => true;
+  
+  void add_warining(String msg){
+    shadowRoot.query('.panel-body').children.add(
+      new DivElement()
+        ..classes.addAll(['alert', 'alert-danger'])
+        ..text = msg);
+  }
+  
+  void clear() {
+    shadowRoot.query('.panel-body').children.clear();
+  }
 }
