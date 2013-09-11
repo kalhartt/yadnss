4dc548c2c724fefae5bff13e7808d63a640e398c
Alex Hartsell <kalhartt@gmail.com>
2013-09-10 21:57:22 -0400

    skill_point panel implemented

--- pure_js/static/js/main.js
+++ pure_js/static/js/main.js
@@ -1,6 +1,16 @@
 var model = {};
+var skill_points;
+
+connect_elements = function() {
+    skill_points = document.querySelector("skill-points");
+    skill_points.set_labels(model.job_byindx);
+}
+
 document.addEventListener('WebComponentsReady', function() {
     var ajax = document.querySelector("polymer-ajax");
-    ajax.addEventListener("polymer-response", function(e) { model.parse(e.detail.response); })
-    ajax.go();
+    ajax.addEventListener("polymer-response", function(e) {
+        model.parse(e.detail.response);
+        connect_elements();
+    });
 });
+
