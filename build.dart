c8df3f1be2e8452143c444242f9f450b35290fb1
Alex Hartsell <kalhartt@gmail.com>
2013-09-15 12:27:13 -0400

    Remove dart version

--- build.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'package:polymer/component_build.dart';     
-import 'package:polymer/deploy.dart' as deploy;
-        
-main() {
-  build(new Options().arguments, ['web/dnskills.html']).then((_) => deploy.main());
-}
