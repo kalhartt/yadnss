3da049ea2c6b63424044adb9b5d3181865a08edb
kalhartt <kalhartt@gmail.com>
2013-09-27 19:14:47 +0000

    JSON url requires only jobhash

--- django/dragonnest/urls.py
+++ django/dragonnest/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^(?P<orientation>landscape|portrait)/(?P<hash>[0-9A-Za-z\-_]+\.[0-9A-Za-z\-_]+)$', ImageView.as_view() ),
-    url(r'^api/(?P<hash>[0-9A-Za-z\-_]+\.[0-9A-Za-z\-_]+)', JSONView.as_view(), name='api'),
+    url(r'^api/(?P<hash>[0-9A-Za-z\-_]*)', JSONView.as_view(), name='api'),
     url(r'^(?P<hash>[0-9A-Za-z\-_]+\.[0-9A-Za-z\-_]+)', TemplateView.as_view(template_name = 'layout_collapse.html')),
     url(r'^', TemplateView.as_view(template_name = 'layout_collapse.html')),
 )
